af3b7369451af536f637405cc0369bceaf6cd775
miguelipi <miguel06ipi@gmail.com>
2025-05-16 13:26:18 +0200

    equiposFiltroYPaginacion

--- src/app/pages/equipos/equipos-page.component.ts
+++ src/app/pages/equipos/equipos-page.component.ts
@@ -1,7 +1,82 @@
-import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { EquiposService } from "../../services/equipos.service";
 
 @Component({
-    templateUrl: './equipos-page.component.html',
+  templateUrl: './equipos-page.component.html',
+  styleUrls: ['./equipos-page.component.scss'],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
 })
-export class EquiposPageComponent {
+export class EquiposPageComponent implements OnInit {
+
+  equipos: any[] = [];
+  seasons: number[] = [];
+
+  selectedSeason: string = '';
+  selectedConferencia: string = '';
+  selectedDivision: string = '';
+
+  eastDivisions = ['Atlantic', 'Central', 'Southeast'];
+  westDivisions = ['Northwest', 'Pacific', 'Southwest'];
+
+  currentPage: number = 1;
+  itemsPerPage: number = 9;
+
+  get filteredDivisions(): string[] {
+    if (this.selectedConferencia === 'East') return this.eastDivisions;
+    if (this.selectedConferencia === 'West') return this.westDivisions;
+    return [];
+  }
+
+  get filteredEquipos(): any[] {
+    return this.equipos.filter(e => {
+      const matchConf = this.selectedConferencia ? e.leagues?.standard?.conference === this.selectedConferencia : true;
+      const matchDiv = this.selectedDivision ? e.leagues?.standard?.division === this.selectedDivision : true;
+      return matchConf && matchDiv;
+    });
+  }
+
+  get pagedEquipos(): any[] {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    return this.filteredEquipos.slice(start, start + this.itemsPerPage);
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.filteredEquipos.length / this.itemsPerPage);
+  }
+
+  constructor(private equiposService: EquiposService) {}
+
+  ngOnInit(): void {
+    this.cargarFiltros();
+  }
+
+  cargarFiltros(): void {
+    this.equiposService.getEquipos().subscribe(data => {
+      this.equipos = data.response;
+    });
+
+    this.equiposService.getSeasons().subscribe(data => {
+      this.seasons = data.response;
+    });
+  }
+
+  onConferenciaChange(): void {
+    this.selectedDivision = '';
+    this.currentPage = 1;
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+  }
+
+  prevPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
 }
---
+++ src/app/services/equipos.service.ts
@@ -0,0 +1,26 @@
+    import { Injectable } from '@angular/core';
+    import { HttpClient, HttpHeaders } from '@angular/common/http';
+    import { Observable, map, switchMap } from 'rxjs';
+
+    @Injectable({
+    providedIn: 'root'
+    })
+    export class EquiposService {
+
+    private baseUrl = 'https://api-nba-v1.p.rapidapi.com';
+    private headers = new HttpHeaders({
+        'X-RapidAPI-Key': '6dc5174fe0msh92cd3293e89487dp196976jsn36819e90cab2', 
+        'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+    });
+
+        getEquipos(): Observable<any> {
+            return this.http.get<any>(`${this.baseUrl}/teams/`, { headers: this.headers });
+        }
+
+        getSeasons(): Observable<any> {
+            return this.http.get<any>(`${this.baseUrl}/seasons/`, { headers: this.headers });
+        }
+
+    constructor(private http: HttpClient) {}
+        
+    }
